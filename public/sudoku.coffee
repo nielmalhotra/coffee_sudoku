53dbf2b3ab9b5b9d507fb656bba8e2af68601508
Pavan <pavan.malhotra93@gmail.com>
2013-01-10 00:15:26 -0500

    Added some more solvers

--- public/sudoku.coffee
+++ public/sudoku.coffee
@@ -2,7 +2,7 @@
 #starting by taking the boxes from left to right, starting from the top row down to the bottom row.
 #Same method with "input" gets all inputs in same order
 
-#creates a blank sudoku grid
+#updates the HTML to show a blank sudoku grid
 window.draw = ->
   #gets all the rows of the sudoku grid from top to bottom
   rows = document.getElementsByTagName("tr")
@@ -56,6 +56,26 @@ window.get_input = ->
       else
         grid[rows][columns] = parseInt(inputs[get_inputs].value)  
       get_inputs++
+
+#these next lines are for testing, I just define the grid in code
+
+  grid = []
+
+  row1 = [1, 2, 3, 4, 5, 6, 7, 8,  one_to_nine()]
+  row2 = [2, 3, 4, 5, 6, 7, 8, 9,  one_to_nine()]
+  row3 = [3, 4, 5, 6, 7, 8, 9, 1,  one_to_nine()]
+  row4 = [4, 5, 6, 7, 8, 9, 1, 2,  one_to_nine()]
+  row5 = [5, 6, 7, 8, 9, 1, 2, 3,  one_to_nine()]
+  row6 = [6, 7, 8, 9, 1, 2, 3, 4,  one_to_nine()]
+  row7 = [7, 8, 9, 1, 2, 3, 4, 5,  one_to_nine()]
+  row8 = [8, 9, 1, 2, 3, 4, 5, 6,  one_to_nine()]
+  row9 = [9, 1, 2, 3, 4, 5, 6, 7, one_to_nine()]
+  grid = [row1,row2,row3,row4,row5,row6,row7,row8,row9]
+
+
+
+  
+
   solved = solve(grid)
   output_solved(solved)
 
@@ -66,16 +86,30 @@ solve = (grid) ->
   #removes values from the arrays at unsolved spots, and returns the new grid array
   #Then add it to the solvers array  
   solvers = [row_removing]
+  console.log("COCK")
+  console.log(grid[0][8])
+  console.log(grid[1][8])
+  console.log(grid[2][8])
+  console.log(grid[3][8])
+  console.log(grid[4][8])
+  console.log(grid[5][8])
+  console.log("COCK")
   while(solved(grid) == false)
     for solve in [0..(solvers.length - 1)] by 1
       grid = solvers[solve](grid)
+    console.log(grid[0][8])
+    console.log(grid[1][8])
+    console.log(grid[2][8])
+    console.log(grid[3][8])
+    console.log(grid[4][8])
+    console.log(grid[5][8])
     update_grid(grid)
   return grid
 
 #returns an array with the numbers 1 through nine
 one_to_nine = ->
   one_to_nine_array = []
-  for counter in [0..8]
+  for counter in [0..8] by 1
     one_to_nine_array[counter] = counter + 1
   return one_to_nine_array
 
@@ -84,7 +118,7 @@ update_grid = (grid) ->
   for rows in [0..8] by 1
     for columns in [0..8] by 1
        if(Array.isArray(grid[rows][columns]))
-         if(grid[rows][columns].length = 1)
+         if(grid[rows][columns].length == 1)
            grid[rows][columns] = grid[rows][columns][0]
   return grid
 
@@ -95,3 +129,39 @@ solved = (grid) ->
       if(Array.isArray(grid[rows][columns]))
         return false
   return true
+
+#updates the HTML to show the solved sudoku puzzle
+output_solved = (solved) ->
+  boxes = document.getElementsByTagName("th")
+  box_counter = 0
+  for rows in [0..8] by 1
+    for columns in [0..8] by 1
+      boxes[box_counter].innerHTML = solved[rows][columns]
+      box_counter++
+
+#removes possible values from a position if that value appears in the same row
+row_removing = (grid) ->
+  console.log("MOTHERFUCKER")
+  for rows in [0..8] by 1
+    for columns in [0..8] by 1
+      console.log(grid[rows][columns])
+      if(Array.isArray(grid[rows][columns]))
+        console.log("PENIS")
+        for col_vals in [0..8] by 1
+          if((col_vals == columns) or (Array.isArray(grid[rows][col_vals])))
+            continue
+          grid[rows][columns] = remove_value(grid[rows][col_vals], grid[rows][columns])
+  return grid
+
+#removes the value val from old_array and returns the updated array without val
+remove_value = (val, old_array) ->
+  console.log("FUCK")
+  console.log(old_array)
+  updated_array = []
+  for counter in [0..(old_array.length - 1)] by 1
+    if(old_array[counter] == val)
+      continue
+    updated_array.push(old_array[counter])
+  console.log(updated_array)
+  return updated_array
+
--- public/sudoku.js
+++ public/sudoku.js
@@ -1,5 +1,5 @@
 (function() {
-  var one_to_nine, solve, solved, update_grid;
+  var one_to_nine, output_solved, remove_value, row_removing, solve, solved, update_grid;
 
   window.draw = function() {
     var boxes, i, q, rows, _ref, _ref2, _results;
@@ -48,7 +48,7 @@
   };
 
   window.get_input = function() {
-    var columns, create_grid, get_inputs, grid, inputs, rows, solved;
+    var columns, create_grid, get_inputs, grid, inputs, row1, row2, row3, row4, row5, row6, row7, row8, row9, rows, solved;
     inputs = document.getElementsByClassName("input");
     grid = [];
     for (create_grid = 0; create_grid <= 8; create_grid += 1) {
@@ -65,6 +65,17 @@
         get_inputs++;
       }
     }
+    grid = [];
+    row1 = [1, 2, 3, 4, 5, 6, 7, 8, one_to_nine()];
+    row2 = [2, 3, 4, 5, 6, 7, 8, 9, one_to_nine()];
+    row3 = [3, 4, 5, 6, 7, 8, 9, 1, one_to_nine()];
+    row4 = [4, 5, 6, 7, 8, 9, 1, 2, one_to_nine()];
+    row5 = [5, 6, 7, 8, 9, 1, 2, 3, one_to_nine()];
+    row6 = [6, 7, 8, 9, 1, 2, 3, 4, one_to_nine()];
+    row7 = [7, 8, 9, 1, 2, 3, 4, 5, one_to_nine()];
+    row8 = [8, 9, 1, 2, 3, 4, 5, 6, one_to_nine()];
+    row9 = [9, 1, 2, 3, 4, 5, 6, 7, one_to_nine()];
+    grid = [row1, row2, row3, row4, row5, row6, row7, row8, row9];
     solved = solve(grid);
     return output_solved(solved);
   };
@@ -72,10 +83,24 @@
   solve = function(grid) {
     var solve, solvers, _ref;
     solvers = [row_removing];
+    console.log("COCK");
+    console.log(grid[0][8]);
+    console.log(grid[1][8]);
+    console.log(grid[2][8]);
+    console.log(grid[3][8]);
+    console.log(grid[4][8]);
+    console.log(grid[5][8]);
+    console.log("COCK");
     while (solved(grid) === false) {
       for (solve = 0, _ref = solvers.length - 1; solve <= _ref; solve += 1) {
         grid = solvers[solve](grid);
       }
+      console.log(grid[0][8]);
+      console.log(grid[1][8]);
+      console.log(grid[2][8]);
+      console.log(grid[3][8]);
+      console.log(grid[4][8]);
+      console.log(grid[5][8]);
       update_grid(grid);
     }
     return grid;
@@ -84,7 +109,7 @@
   one_to_nine = function() {
     var counter, one_to_nine_array;
     one_to_nine_array = [];
-    for (counter = 0; counter <= 8; counter++) {
+    for (counter = 0; counter <= 8; counter += 1) {
       one_to_nine_array[counter] = counter + 1;
     }
     return one_to_nine_array;
@@ -95,7 +120,7 @@
     for (rows = 0; rows <= 8; rows += 1) {
       for (columns = 0; columns <= 8; columns += 1) {
         if (Array.isArray(grid[rows][columns])) {
-          if ((grid[rows][columns].length = 1)) {
+          if (grid[rows][columns].length === 1) {
             grid[rows][columns] = grid[rows][columns][0];
           }
         }
@@ -112,6 +137,58 @@
       }
     }
     return true;
+  };
+
+  output_solved = function(solved) {
+    var box_counter, boxes, columns, rows, _results;
+    boxes = document.getElementsByTagName("th");
+    box_counter = 0;
+    _results = [];
+    for (rows = 0; rows <= 8; rows += 1) {
+      _results.push((function() {
+        var _results2;
+        _results2 = [];
+        for (columns = 0; columns <= 8; columns += 1) {
+          boxes[box_counter].innerHTML = solved[rows][columns];
+          _results2.push(box_counter++);
+        }
+        return _results2;
+      })());
+    }
+    return _results;
+  };
+
+  row_removing = function(grid) {
+    var col_vals, columns, rows;
+    console.log("MOTHERFUCKER");
+    for (rows = 0; rows <= 8; rows += 1) {
+      for (columns = 0; columns <= 8; columns += 1) {
+        console.log(grid[rows][columns]);
+        if (Array.isArray(grid[rows][columns])) {
+          console.log("PENIS");
+          for (col_vals = 0; col_vals <= 8; col_vals += 1) {
+            if ((col_vals === columns) || (Array.isArray(grid[rows][col_vals]))) {
+              continue;
+            }
+            grid[rows][columns] = remove_value(grid[rows][col_vals], grid[rows][columns]);
+          }
+        }
+      }
+    }
+    return grid;
+  };
+
+  remove_value = function(val, old_array) {
+    var counter, updated_array, _ref;
+    console.log("FUCK");
+    console.log(old_array);
+    updated_array = [];
+    for (counter = 0, _ref = old_array.length - 1; counter <= _ref; counter += 1) {
+      if (old_array[counter] === val) continue;
+      updated_array.push(old_array[counter]);
+    }
+    console.log(updated_array);
+    return updated_array;
   };
 
 }).call(this);

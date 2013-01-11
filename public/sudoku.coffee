2737adbbc07d2968e29ea5f7967b6e6abaa55c03
Pavan <pavan.malhotra93@gmail.com>
2013-01-10 22:24:43 -0500

    Added many more solvers and added a contradiction checker

--- public/sudoku.coffee
+++ public/sudoku.coffee
@@ -42,7 +42,8 @@ window.draw = ->
       else
         boxes[i].style.backgroundColor = "#DEF3CA"
 
-#gets inputs, puts them into a 2d array and calls solve on the 2d array
+#The central data structure is a 3d array of the puzzle. Think of it as a 2d array of the grid.
+#If the value of a position is known, put that integer in its position. If the value of a position is not known, put an array of possibilities in that position.
 window.get_input = ->
   inputs = document.getElementsByClassName("input")
   grid = []
@@ -57,53 +58,49 @@ window.get_input = ->
         grid[rows][columns] = parseInt(inputs[get_inputs].value)  
       get_inputs++
 
-#these next lines are for testing, I just define the grid in code
+#These next lines are for testing. For convenience, you can define the grid in code
 
+  b = one_to_nine()
   grid = []
 
-  row1 = [1, 2, 3, 4, 5, 6, 7, 8,  one_to_nine()]
-  row2 = [2, 3, 4, 5, 6, 7, 8, 9,  one_to_nine()]
-  row3 = [3, 4, 5, 6, 7, 8, 9, 1,  one_to_nine()]
-  row4 = [4, 5, 6, 7, 8, 9, 1, 2,  one_to_nine()]
-  row5 = [5, 6, 7, 8, 9, 1, 2, 3,  one_to_nine()]
-  row6 = [6, 7, 8, 9, 1, 2, 3, 4,  one_to_nine()]
-  row7 = [7, 8, 9, 1, 2, 3, 4, 5,  one_to_nine()]
-  row8 = [8, 9, 1, 2, 3, 4, 5, 6,  one_to_nine()]
-  row9 = [9, 1, 2, 3, 4, 5, 6, 7, one_to_nine()]
-  grid = [row1,row2,row3,row4,row5,row6,row7,row8,row9]
-
-
-
-  
+  row1 = [1,b,b,b,b,b,b,b,b]
+  row2 = [2,b,b,b,b,b,b,b,b]
+  row3 = [3,b,b,b,b,b,b,b,b]
+  row4 = [4,b,b,b,b,b,b,b,b]
+  row5 = [5,b,b,b,b,b,b,b,b]
+  row6 = [b,b,b,b,b,b,b,b,b]
+  row7 = [b,b,b,b,b,b,b,b,b]
+  row8 = [b,b,b,b,b,b,b,b,b]
+  row9 = [b,b,b,b,b,6,7,8,9]
+  grid = [row1, row2, row3, row4, row5, row6,row7,row8,row9]
 
   solved = solve(grid)
+  if(solved == false)
+    output_contradiction()    
   output_solved(solved)
 
 #solves the sudoku puzzle and returns it as a 2d array
-solve = (grid) ->
+#if there is a contradiction, then it returns false
+solve = (grid) ->        
   #This project is not complete.
   #To add new logic just define a function that accepts grid,
   #removes values from the arrays at unsolved spots, and returns the new grid array
   #Then add it to the solvers array  
-  solvers = [row_removing]
-  console.log("COCK")
-  console.log(grid[0][8])
-  console.log(grid[1][8])
-  console.log(grid[2][8])
-  console.log(grid[3][8])
-  console.log(grid[4][8])
-  console.log(grid[5][8])
-  console.log("COCK")
+  rounds_without_updates = 0
+  solvers = [row_removing, column_removing, box_removing, row_possibilities, column_possibilities, box_possibilities]
   while(solved(grid) == false)
+    grid_snapshot = grid
+    if (has_contradiction(grid))
+      return false
     for solve in [0..(solvers.length - 1)] by 1
       grid = solvers[solve](grid)
-    console.log(grid[0][8])
-    console.log(grid[1][8])
-    console.log(grid[2][8])
-    console.log(grid[3][8])
-    console.log(grid[4][8])
-    console.log(grid[5][8])
-    update_grid(grid)
+      grid = update_grid(grid)
+    if(has_changed(grid_snapshot, grid) == false)
+      rounds_without_updates++
+    else
+      rounds_without_updates = 0
+    if(rounds_without_updates == 2)
+      console.log("HAPPY HAPPY JOY JOY")
   return grid
 
 #returns an array with the numbers 1 through nine
@@ -139,14 +136,17 @@ output_solved = (solved) ->
       boxes[box_counter].innerHTML = solved[rows][columns]
       box_counter++
 
+#updates the HTML to show that there was a contradiction
+output_contradiction = ->
+  message = document.getElementById('messages')
+  message.innerHTML = "EPIC FAIL! Your sudoku puzzle is impossible to solve. Click refresh to get a new sudoku board and check to make sure that you entered your sudoku puzzle correctly"
+  
+
 #removes possible values from a position if that value appears in the same row
 row_removing = (grid) ->
-  console.log("MOTHERFUCKER")
   for rows in [0..8] by 1
     for columns in [0..8] by 1
-      console.log(grid[rows][columns])
       if(Array.isArray(grid[rows][columns]))
-        console.log("PENIS")
         for col_vals in [0..8] by 1
           if((col_vals == columns) or (Array.isArray(grid[rows][col_vals])))
             continue
@@ -155,13 +155,177 @@ row_removing = (grid) ->
 
 #removes the value val from old_array and returns the updated array without val
 remove_value = (val, old_array) ->
-  console.log("FUCK")
-  console.log(old_array)
   updated_array = []
   for counter in [0..(old_array.length - 1)] by 1
     if(old_array[counter] == val)
       continue
     updated_array.push(old_array[counter])
-  console.log(updated_array)
   return updated_array
+
+#removes possible values from a position if that value appears in the same column
+column_removing = (grid) ->
+  for rows in [0..8] by 1
+    for columns in [0..8] by 1
+      if(Array.isArray(grid[rows][columns]))
+        for row_vals in [0..8] by 1
+          if((row_vals == rows) or (Array.isArray(grid[row_vals][columns])))
+            continue
+          grid[rows][columns] = remove_value(grid[row_vals][columns], grid[rows][columns])
+  return grid
+
+#removes possible values from all position if that value appears in the same 3x3 box
+box_removing = (grid) ->
+  for rows in [0..8] by 1
+    for columns in [0..8] by 1
+      if(Array.isArray(grid[rows][columns]))
+        grid = box_solve(grid, rows, columns)
+  return grid
+
+#returns an updated grid with possibilities removed that occur in the same 3x3 box as position specified by row rows and column columns
+box_solve = (grid, rows, columns) ->  
+  row_range = box_range(rows)
+  column_range = box_range(columns)
+  for row_count in row_range by 1
+    for column_count in column_range by 1
+      if(((row_count == rows) and (column_count == columns)) or Array.isArray(grid[row_count][column_count]))
+        continue
+      grid[rows][columns] = remove_value(grid[row_count][column_count], grid[rows][columns])
+  return grid
+
+#gets the range of positions (0-2, 3-5, or 6-8) that a position belongs to
+box_range = (position) ->
+  if((position >= 0) and (position <= 2))
+    return [0,1,2]
+  else if ((position >= 3) and (position <= 5))
+    return [3,4,5]
+  else
+    return [6,7,8]
+
+#checks all positions and sets the value of a position if it's the only position in the row with a possible value
+row_possibilities = (grid) ->
+  for rows in [0..8] by 1
+    for columns in [0..8] by 1
+      if(Array.isArray(grid[rows][columns]))
+        for possible_val in grid[rows][columns] by 1
+          grid = unique_row_possibility(grid, rows, columns, possible_val)
+          if(Array.isArray(grid[rows][columns]) == false)
+            break
+  return grid
+        
+#Checks position at row rows and column columns to see if value possible_val is a unique possibility in a row.
+#If it is unique, grid is updated and returned
+unique_row_possibility = (grid, rows, columns, possible_val) ->
+  only_possible_place = true
+  for col_vals in [0..8] by 1
+    if((col_vals == columns) or (Array.isArray(grid[rows][col_vals]) == false))
+      continue
+    for other_possible_val in grid[rows][col_vals]
+      if other_possible_val == possible_val
+        only_possible_place = false
+  if(only_possible_place == true)
+    grid[rows][columns] = possible_val
+  return grid
+
+#checks all possibilities and sets the value of a position if it's the only position in the column with a possible value
+column_possibilities = (grid) ->
+  for rows in [0..8] by 1
+    for columns in [0..8] by 1
+      if(Array.isArray(grid[rows][columns]))
+        for possible_val in grid[rows][columns] by 1
+          grid = unique_column_possibility(grid, rows, columns, possible_val)
+          if(Array.isArray(grid[rows][columns]) == false)
+            break
+  return grid
+
+#Checks position at row rows and column columns to see if value possible_val is a unique possibility in a column.
+#If it is unique, grid is updated and returned
+unique_column_possibility = (grid, rows, columns, possible_val) ->
+  only_possible_place = true
+  for row_vals in [0..8] by 1
+    if((row_vals == rows) or (Array.isArray(grid[row_vals][columns]) == false))
+      continue
+    for other_possible_val in grid[row_vals][columns]
+      if other_possible_val == possible_val
+        only_possible_place = false
+  if(only_possible_place == true)
+    grid[rows][columns] = possible_val
+  return grid
+
+#checks all possibilities and sets the value of a position if it's the only position in a 3x3 box with a possible value
+box_possibilities = (grid) ->
+  for rows in [0..8] by 1
+    for columns in [0..8] by 1
+      if(Array.isArray(grid[rows][columns]))
+        for possible_val in grid[rows][columns] by 1
+          grid = unique_box_possibility(grid, rows, columns, possible_val)
+        if(Array.isArray(grid[rows][columns] == false))
+          break
+  return grid
+
+#Checks position at row rows and column columns to see if value possible_val is a unique possibility in a 3x3 box.
+#If it is unique, grid is updated and returned
+unique_box_possibility = (grid, rows, columns, possible_val) ->
+  only_possible_place = true
+  row_range = box_range(rows)
+  column_range = box_range(columns)
+  for row_count in row_range by 1
+    for column_count in column_range by 1
+      if(((row_count == rows) and (column_count == columns)) or (Array.isArray(grid[row_count][column_count]) == false))
+        continue
+      for other_possible_val in grid[row_count][column_count] by 1
+        if other_possible_val == possible_val
+          only_possible_place = false
+  if(only_possible_place == true)
+    grid[rows][columns] = possible_val
+  return grid
+
+#checks if there is a contradiction in the sudoku puzzle and returns true or false
+has_contradiction = (grid) ->
+  for rows in [0..8] by 1
+    for columns in [0..8] by 1
+      check_contradictions = [zero_possibilities, row_repeats, column_repeats, box_repeats]
+      for counter in [0..3] by 1
+        con_exists = check_contradictions[counter](grid, rows, columns)
+        if(con_exists == true)
+          return true
+  return false
+
+#returns true if at position [rows][columns] there are no possibilities
+zero_possibilities = (grid, rows, columns) ->
+  if((Array.isArray(grid[rows][columns])) and (grid[rows][columns].length == 0))
+    return true
+  return false
+
+#returns true if another position in the row rows has the same value as [rows][columns]
+row_repeats = (grid, rows, columns) ->
+  if(Array.isArray(grid[rows][columns]) == false)
+    for column_vals in [0..8] by 1
+      if((column_vals == columns) or (Array.isArray(grid[rows][column_vals])))
+        continue
+      if(grid[rows][column_vals] == grid[rows][columns])
+        return true
+  return false
+
+#returns true if another position in the column columns has the same value as [rows][columns]
+column_repeats = (grid, rows, columns) ->
+  if(Array.isArray(grid[rows][columns]) == false)
+    for row_vals in [0..8] by 1
+      if((row_vals == rows) or (Array.isArray(grid[row_vals][columns])))
+        continue
+      if(grid[row_vals][columns] == grid[rows][columns])
+        return true
+  return false
+
+#returns true if another position in the 3x3 box has the same value as [rows][columns]
+box_repeats = (grid, rows, columns) ->
+  if(Array.isArray(grid[rows][columns]) == false)
+    row_range = box_range(rows)
+    column_range = box_range(columns)
+    for row_count in row_range by 1
+      for column_count in column_range by 1
+        if(((row_count == rows) and (column_count == columns)) or (Array.isArray(grid[row_count][column_count])))
+          continue
+        if(grid[row_count][column_count] == grid[rows][columns])
+          return true
+  return false
 
--- public/sudoku.js
+++ public/sudoku.js
@@ -1,5 +1,5 @@
 (function() {
-  var one_to_nine, output_solved, remove_value, row_removing, solve, solved, update_grid;
+  var box_possibilities, box_range, box_removing, box_repeats, box_solve, column_possibilities, column_removing, column_repeats, has_contradiction, one_to_nine, output_contradiction, output_solved, remove_value, row_possibilities, row_removing, row_repeats, solve, solved, unique_box_possibility, unique_column_possibility, unique_row_possibility, update_grid, zero_possibilities;
 
   window.draw = function() {
     var boxes, i, q, rows, _ref, _ref2, _results;
@@ -48,7 +48,7 @@
   };
 
   window.get_input = function() {
-    var columns, create_grid, get_inputs, grid, inputs, row1, row2, row3, row4, row5, row6, row7, row8, row9, rows, solved;
+    var b, columns, create_grid, get_inputs, grid, inputs, row1, row2, row3, row4, row5, row6, row7, row8, row9, rows, solved;
     inputs = document.getElementsByClassName("input");
     grid = [];
     for (create_grid = 0; create_grid <= 8; create_grid += 1) {
@@ -65,43 +65,33 @@
         get_inputs++;
       }
     }
+    b = one_to_nine();
     grid = [];
-    row1 = [1, 2, 3, 4, 5, 6, 7, 8, one_to_nine()];
-    row2 = [2, 3, 4, 5, 6, 7, 8, 9, one_to_nine()];
-    row3 = [3, 4, 5, 6, 7, 8, 9, 1, one_to_nine()];
-    row4 = [4, 5, 6, 7, 8, 9, 1, 2, one_to_nine()];
-    row5 = [5, 6, 7, 8, 9, 1, 2, 3, one_to_nine()];
-    row6 = [6, 7, 8, 9, 1, 2, 3, 4, one_to_nine()];
-    row7 = [7, 8, 9, 1, 2, 3, 4, 5, one_to_nine()];
-    row8 = [8, 9, 1, 2, 3, 4, 5, 6, one_to_nine()];
-    row9 = [9, 1, 2, 3, 4, 5, 6, 7, one_to_nine()];
+    row1 = [1, b, b, b, b, b, b, b, b];
+    row2 = [2, b, b, b, b, b, b, b, b];
+    row3 = [3, b, b, b, b, b, b, b, b];
+    row4 = [4, b, b, b, b, b, b, b, b];
+    row5 = [5, b, b, b, b, b, b, b, b];
+    row6 = [b, b, b, b, b, b, b, b, b];
+    row7 = [b, b, b, b, b, b, b, b, b];
+    row8 = [b, b, b, b, b, b, b, b, b];
+    row9 = [b, b, b, b, b, 6, 7, 8, 9];
     grid = [row1, row2, row3, row4, row5, row6, row7, row8, row9];
     solved = solve(grid);
+    if (solved === false) output_contradiction();
     return output_solved(solved);
   };
 
   solve = function(grid) {
-    var solve, solvers, _ref;
-    solvers = [row_removing];
-    console.log("COCK");
-    console.log(grid[0][8]);
-    console.log(grid[1][8]);
-    console.log(grid[2][8]);
-    console.log(grid[3][8]);
-    console.log(grid[4][8]);
-    console.log(grid[5][8]);
-    console.log("COCK");
+    var rounds_without_updates, solve, solvers, _ref;
+    rounds_without_updates = 0;
+    solvers = [row_removing, column_removing, box_removing, row_possibilities, column_possibilities, box_possibilities];
     while (solved(grid) === false) {
+      if (has_contradiction(grid)) return false;
       for (solve = 0, _ref = solvers.length - 1; solve <= _ref; solve += 1) {
         grid = solvers[solve](grid);
+        grid = update_grid(grid);
       }
-      console.log(grid[0][8]);
-      console.log(grid[1][8]);
-      console.log(grid[2][8]);
-      console.log(grid[3][8]);
-      console.log(grid[4][8]);
-      console.log(grid[5][8]);
-      update_grid(grid);
     }
     return grid;
   };
@@ -158,14 +148,17 @@
     return _results;
   };
 
+  output_contradiction = function() {
+    var message;
+    message = document.getElementById('messages');
+    return message.innerHTML = "EPIC FAIL! Your sudoku puzzle is impossible to solve. Click refresh to get a new sudoku board and check to make sure that you entered your sudoku puzzle correctly";
+  };
+
   row_removing = function(grid) {
     var col_vals, columns, rows;
-    console.log("MOTHERFUCKER");
     for (rows = 0; rows <= 8; rows += 1) {
       for (columns = 0; columns <= 8; columns += 1) {
-        console.log(grid[rows][columns]);
         if (Array.isArray(grid[rows][columns])) {
-          console.log("PENIS");
           for (col_vals = 0; col_vals <= 8; col_vals += 1) {
             if ((col_vals === columns) || (Array.isArray(grid[rows][col_vals]))) {
               continue;
@@ -180,15 +173,242 @@
 
   remove_value = function(val, old_array) {
     var counter, updated_array, _ref;
-    console.log("FUCK");
-    console.log(old_array);
     updated_array = [];
     for (counter = 0, _ref = old_array.length - 1; counter <= _ref; counter += 1) {
       if (old_array[counter] === val) continue;
       updated_array.push(old_array[counter]);
     }
-    console.log(updated_array);
     return updated_array;
+  };
+
+  column_removing = function(grid) {
+    var columns, row_vals, rows;
+    for (rows = 0; rows <= 8; rows += 1) {
+      for (columns = 0; columns <= 8; columns += 1) {
+        if (Array.isArray(grid[rows][columns])) {
+          for (row_vals = 0; row_vals <= 8; row_vals += 1) {
+            if ((row_vals === rows) || (Array.isArray(grid[row_vals][columns]))) {
+              continue;
+            }
+            grid[rows][columns] = remove_value(grid[row_vals][columns], grid[rows][columns]);
+          }
+        }
+      }
+    }
+    return grid;
+  };
+
+  box_removing = function(grid) {
+    var columns, rows;
+    for (rows = 0; rows <= 8; rows += 1) {
+      for (columns = 0; columns <= 8; columns += 1) {
+        if (Array.isArray(grid[rows][columns])) {
+          grid = box_solve(grid, rows, columns);
+        }
+      }
+    }
+    return grid;
+  };
+
+  box_solve = function(grid, rows, columns) {
+    var column_count, column_range, row_count, row_range, _i, _j, _len, _len2, _step, _step2;
+    row_range = box_range(rows);
+    column_range = box_range(columns);
+    for (_i = 0, _len = row_range.length, _step = 1; _i < _len; _i += _step) {
+      row_count = row_range[_i];
+      for (_j = 0, _len2 = column_range.length, _step2 = 1; _j < _len2; _j += _step2) {
+        column_count = column_range[_j];
+        if (((row_count === rows) && (column_count === columns)) || Array.isArray(grid[row_count][column_count])) {
+          continue;
+        }
+        grid[rows][columns] = remove_value(grid[row_count][column_count], grid[rows][columns]);
+      }
+    }
+    return grid;
+  };
+
+  box_range = function(position) {
+    if ((position >= 0) && (position <= 2)) {
+      return [0, 1, 2];
+    } else if ((position >= 3) && (position <= 5)) {
+      return [3, 4, 5];
+    } else {
+      return [6, 7, 8];
+    }
+  };
+
+  row_possibilities = function(grid) {
+    var columns, possible_val, rows, _i, _len, _ref, _step;
+    for (rows = 0; rows <= 8; rows += 1) {
+      for (columns = 0; columns <= 8; columns += 1) {
+        if (Array.isArray(grid[rows][columns])) {
+          _ref = grid[rows][columns];
+          for (_i = 0, _len = _ref.length, _step = 1; _i < _len; _i += _step) {
+            possible_val = _ref[_i];
+            grid = unique_row_possibility(grid, rows, columns, possible_val);
+            if (Array.isArray(grid[rows][columns]) === false) break;
+          }
+        }
+      }
+    }
+    return grid;
+  };
+
+  unique_row_possibility = function(grid, rows, columns, possible_val) {
+    var col_vals, only_possible_place, other_possible_val, _i, _len, _ref;
+    only_possible_place = true;
+    for (col_vals = 0; col_vals <= 8; col_vals += 1) {
+      if ((col_vals === columns) || (Array.isArray(grid[rows][col_vals]) === false)) {
+        continue;
+      }
+      _ref = grid[rows][col_vals];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        other_possible_val = _ref[_i];
+        if (other_possible_val === possible_val) only_possible_place = false;
+      }
+    }
+    if (only_possible_place === true) grid[rows][columns] = possible_val;
+    return grid;
+  };
+
+  column_possibilities = function(grid) {
+    var columns, possible_val, rows, _i, _len, _ref, _step;
+    for (rows = 0; rows <= 8; rows += 1) {
+      for (columns = 0; columns <= 8; columns += 1) {
+        if (Array.isArray(grid[rows][columns])) {
+          _ref = grid[rows][columns];
+          for (_i = 0, _len = _ref.length, _step = 1; _i < _len; _i += _step) {
+            possible_val = _ref[_i];
+            grid = unique_column_possibility(grid, rows, columns, possible_val);
+            if (Array.isArray(grid[rows][columns]) === false) break;
+          }
+        }
+      }
+    }
+    return grid;
+  };
+
+  unique_column_possibility = function(grid, rows, columns, possible_val) {
+    var only_possible_place, other_possible_val, row_vals, _i, _len, _ref;
+    only_possible_place = true;
+    for (row_vals = 0; row_vals <= 8; row_vals += 1) {
+      if ((row_vals === rows) || (Array.isArray(grid[row_vals][columns]) === false)) {
+        continue;
+      }
+      _ref = grid[row_vals][columns];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        other_possible_val = _ref[_i];
+        if (other_possible_val === possible_val) only_possible_place = false;
+      }
+    }
+    if (only_possible_place === true) grid[rows][columns] = possible_val;
+    return grid;
+  };
+
+  box_possibilities = function(grid) {
+    var columns, possible_val, rows, _i, _len, _ref, _step;
+    for (rows = 0; rows <= 8; rows += 1) {
+      for (columns = 0; columns <= 8; columns += 1) {
+        if (Array.isArray(grid[rows][columns])) {
+          _ref = grid[rows][columns];
+          for (_i = 0, _len = _ref.length, _step = 1; _i < _len; _i += _step) {
+            possible_val = _ref[_i];
+            grid = unique_box_possibility(grid, rows, columns, possible_val);
+          }
+          if (Array.isArray(grid[rows][columns] === false)) break;
+        }
+      }
+    }
+    return grid;
+  };
+
+  unique_box_possibility = function(grid, rows, columns, possible_val) {
+    var column_count, column_range, only_possible_place, other_possible_val, row_count, row_range, _i, _j, _k, _len, _len2, _len3, _ref, _step, _step2, _step3;
+    only_possible_place = true;
+    row_range = box_range(rows);
+    column_range = box_range(columns);
+    for (_i = 0, _len = row_range.length, _step = 1; _i < _len; _i += _step) {
+      row_count = row_range[_i];
+      for (_j = 0, _len2 = column_range.length, _step2 = 1; _j < _len2; _j += _step2) {
+        column_count = column_range[_j];
+        if (((row_count === rows) && (column_count === columns)) || (Array.isArray(grid[row_count][column_count]) === false)) {
+          continue;
+        }
+        _ref = grid[row_count][column_count];
+        for (_k = 0, _len3 = _ref.length, _step3 = 1; _k < _len3; _k += _step3) {
+          other_possible_val = _ref[_k];
+          if (other_possible_val === possible_val) only_possible_place = false;
+        }
+      }
+    }
+    if (only_possible_place === true) grid[rows][columns] = possible_val;
+    return grid;
+  };
+
+  has_contradiction = function(grid) {
+    var check_contradictions, columns, con_exists, counter, rows;
+    for (rows = 0; rows <= 8; rows += 1) {
+      for (columns = 0; columns <= 8; columns += 1) {
+        check_contradictions = [zero_possibilities, row_repeats, column_repeats, box_repeats];
+        for (counter = 0; counter <= 3; counter += 1) {
+          con_exists = check_contradictions[counter](grid, rows, columns);
+          if (con_exists === true) return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  zero_possibilities = function(grid, rows, columns) {
+    if ((Array.isArray(grid[rows][columns])) && (grid[rows][columns].length === 0)) {
+      return true;
+    }
+    return false;
+  };
+
+  row_repeats = function(grid, rows, columns) {
+    var column_vals;
+    if (Array.isArray(grid[rows][columns]) === false) {
+      for (column_vals = 0; column_vals <= 8; column_vals += 1) {
+        if ((column_vals === columns) || (Array.isArray(grid[rows][column_vals]))) {
+          continue;
+        }
+        if (grid[rows][column_vals] === grid[rows][columns]) return true;
+      }
+    }
+    return false;
+  };
+
+  column_repeats = function(grid, rows, columns) {
+    var row_vals;
+    if (Array.isArray(grid[rows][columns]) === false) {
+      for (row_vals = 0; row_vals <= 8; row_vals += 1) {
+        if ((row_vals === rows) || (Array.isArray(grid[row_vals][columns]))) {
+          continue;
+        }
+        if (grid[row_vals][columns] === grid[rows][columns]) return true;
+      }
+    }
+    return false;
+  };
+
+  box_repeats = function(grid, rows, columns) {
+    var column_count, column_range, row_count, row_range, _i, _j, _len, _len2, _step, _step2;
+    if (Array.isArray(grid[rows][columns]) === false) {
+      row_range = box_range(rows);
+      column_range = box_range(columns);
+      for (_i = 0, _len = row_range.length, _step = 1; _i < _len; _i += _step) {
+        row_count = row_range[_i];
+        for (_j = 0, _len2 = column_range.length, _step2 = 1; _j < _len2; _j += _step2) {
+          column_count = column_range[_j];
+          if (((row_count === rows) && (column_count === columns)) || (Array.isArray(grid[row_count][column_count]))) {
+            continue;
+          }
+          if (grid[row_count][column_count] === grid[rows][columns]) return true;
+        }
+      }
+    }
+    return false;
   };
 
 }).call(this);
